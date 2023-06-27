3d18336b8188e6b75045977bec9f519bf02596fd
janejs92 <137512387+janejs92@users.noreply.github.com>
2023-06-27 19:42:26 -0400

    Add files via upload

---
+++ Example/Tests/Tests.swift
@@ -0,0 +1,8 @@
+import XCTest
+import ColorCompatibility
+
+class Tests: XCTestCase {
+    func testExample() {
+        XCTAssertNotNil(ColorCompatibility.systemBackground, "This is just here to make sure everything compiles")
+    }
+}
